a2bf6c5ba9c0003bdab1c22ea1e26dd7058504de
mote <mote@ten.(none)>
2013-02-02 14:54:18 -0800

    Added simple web frontend using web.py, added .gitignore

---
+++ server/graphme.py
@@ -0,0 +1,3 @@
+class Graph:
+  def GET(self, name=''):
+    return 'I am a graph for %s' % name
---
+++ server/server.py
@@ -0,0 +1,27 @@
+import web
+from graphme import Graph
+
+web.internalerror = web.debugerror
+
+_t_globals = {
+    'user': 'mote',
+    }
+_render = web.template.render('templates/', cache=False, globals=_t_globals)
+
+class View:
+  def GET(self, args=''):
+    return 'hello world'
+
+class RobotsTxt:
+  def GET(self):
+    return ''
+
+if __name__ == '__main__':
+  urls = [
+      '', 'View',
+      '/', 'View',
+      '/graph/(.+)', 'Graph',
+      ]
+  application = web.application(urls, globals())
+  application.run()
+
