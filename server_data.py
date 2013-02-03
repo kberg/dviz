37e032ef0f069f2d58a9a45858f6a366423905d8
mote <mote@ten.(none)>
2013-02-02 16:33:18 -0800

    added header

--- server_data.py
+++ server_data.py
@@ -4,17 +4,20 @@ import datastore
 class Data:
   def GET(self, names):
     d = datastore.Datastore()
+    header = 'date,%s\n' % names
+
     names = names.split(',')
     if len(names) == 0:
       return ''
     if len(names) == 1:
       data = d.get(names[0])
       data = [','.join([str(dd) for dd in d]) for d in data]
-      return '\n'.join(data)
+      return header + '\n'.join(data)
 
 
 
     data = d.getmany(names)
     data = [','.join([str(dd) for dd in d]) for d in data]
-    return '\n'.join(data)
+
+    return header + '\n'.join(data)
 
