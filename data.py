e1dc3df66b7cb8b448aed0dc63fcf67ef753374e
mote <mote@ten.(none)>
2013-02-18 23:13:24 -0800

    added user dimension, and simple web page for adding new user secret

--- data.py
+++ data.py
@@ -4,9 +4,20 @@ import sys
 from google.appengine.ext import db
 from google.appengine.ext import ndb
 
+
+class User(db.Model):
+  uid = db.StringProperty()      # username
+  secret = db.StringProperty()   # secret key for POSTing.
+                                 # TODO: salt.
+
+
 class Series(db.Model):
   name = db.StringProperty()
   timestamp = db.DateTimeProperty(auto_now=True)
+  owner = db.ReferenceProperty(User)
+  # TODO: add visibility rules.
+  # Maybe just a bool externally-visible flag, or maybe something more
+  # complicated like a list of users that have read/write privs.
 
 
 class SeriesData(db.Model):
@@ -15,17 +26,70 @@ class SeriesData(db.Model):
   value = db.FloatProperty()
 
 
-class User(db.Model):
-  uid = db.StringProperty()      # username
-  secret = db.StringProperty()   # secret key for POSTing.
-  # TODO: need to do stuff with this class.
+def get_user(user_id='', secret=''):
+  if secret:
+    return get_user_by_secret(secret)
+  if user_id:
+    return get_user_by_id(user_id)
+  return
 
 
-def get_series_by_name(name):
+def get_user_by_id(user_id):
+  try:
+    return User.gql('WHERE uid =:1', user_id)[0]
+  except IndexError:
+    return None
+
+
+def get_user_by_secret(secret):
+  try:
+    return User.gql('WHERE secret =:1', secret)[0]
+  except IndexError:
+    return None
+
+
+def add_user(user_id, secret):
+  # TODO: should raise exceptions here probably.
+  # Don't create a dupe user
+  if get_user_by_id(user_id):
+    return
+  # likewise, secret should be unique.
+  if get_user_by_secret(secret):
+    return
+  u = User(uid=user_id, secret=secret)
+  u.put()
+
+
+def get_all_users():
+  return User.all().run()
+
+
+def get_series_by_name(name, user=None):
+  # TODO: enforce user ACL.
   try:
     return Series.gql('WHERE name =:1', name)[0]
   except IndexError:
     return None
+
+
+def get_or_add_series(user, name):
+  """Gets a series, or adds it if it doesn't exist."""
+  series = get_series_by_name(user, name)
+  if not series:
+    series = Series(name=name, owner=user)
+    series.put()
+  return series
+
+
+def get_all_series():
+  return Series.all().run()
+
+
+def get_all_series_for_user(user_id):
+  u = get_user_by_id(user_id)
+  if not u:
+    return []
+  return Series.all().filter('owner =', user).run() 
 
 
 def get_series_data(name, since):
@@ -58,21 +122,12 @@ def get_multiple_series_data(names, since):
       'timestamp > ', since).order('-timestamp')
 
 
-def get_or_add_series(name):
-  """Gets a series, or adds it if it doesn't exist."""
-  series = get_series_by_name(name)
+def add(name, value, user_id='', secret='', timestamp=None):
+  user = get_user(user_id, secret_)
+  series = get_or_add_series(user, name)
   if not series:
-    series = Series(name=name)
-    series.put()
-  return series
-
-
-def get_all_series():
-  return Series.all().run()
-
-
-def add(name, value, timestamp=None):
-  series = get_or_add_series(name)
+    # Bad user?  fail silently.
+    return
   d = SeriesData(series=series, value=value)
   if not timestamp:
     timestamp = datetime.datetime.now()
--- dviz.py
+++ dviz.py
@@ -2,17 +2,48 @@
 #
 
 import datetime
-import random
 import os
+import random
+import sys
 import webapp2
 
 import data
 
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.out.write('Hello world.')
+
+class User(webapp2.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'templates/user.html')
+    user = users.get_current_user()
+    if user:
+      link = users.create_logout_url(self.request.uri)
+      link_text = 'Logout'
+    else:
+      link = users.create_login_url(self.request.uri)
+      link_text = 'Login'
+
+    if user:
+      user_id = user.user_id()
+    else:
+      user_id = ''
+    template_values = {
+        'link': link,
+        'link_text' : link_text,
+        'users': data.get_all_users(),
+        'user_id' : user_id
+        }
+    self.response.out.write(template.render(path, template_values))
+
+  def post(self):
+    #sys.stderr.write(' '.join(self.request.get_all()))
+    user_id = self.request.get('user_id')
+    secret = self.request.get('secret')
+    data.add_user(user_id, secret)
 
 
 class Graph(webapp2.RequestHandler):
@@ -165,6 +196,7 @@ class Series(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
   ('/', MainPage),
+  ('/user', User),
   ('/data', Data),
   ('/data/(.+)/(.+)', Data),
   ('/list', List),
