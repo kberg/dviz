4e0d4382ce9fe05b1c2f14b072e4133673820ed1
Robert Konigsberg <konigsberg@gmail.com>
2013-03-03 17:59:51 -0500

    Move addrandom to its own module.

--- dviz.py
+++ dviz.py
@@ -3,7 +3,6 @@
 
 import datetime
 import os
-import random
 import sys
 import webapp2
 
@@ -11,6 +10,8 @@ import data
 
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+
+from handlers import addrandom
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
@@ -44,7 +45,6 @@ class User(webapp2.RequestHandler):
     user_id = self.request.get('user_id')
     secret = self.request.get('secret')
     data.add_user(user_id, secret)
-
 
 class Graph(webapp2.RequestHandler):
   def get(self, names):
@@ -153,31 +153,6 @@ class List(webapp2.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
     self.response.out.write(template.render(path, template_values))
 
-
- 
-class AddRandom(webapp2.RequestHandler):
-  def get(self):
-   self.response.out.write('adding random data.')
-   now = datetime.datetime.now()
-   for i in range(20):
-
-     # add a few in last hour, day, week, month, year.
-     hour_secs = 60*60
-     day_secs = 24*hour_secs
-     week_secs = 7*day_secs
-     month_secs = 30*day_secs
-     year_secs = 365*day_secs
-
-     for ago in (hour_secs, day_secs, week_secs, month_secs, year_secs):
-       data.add('first',
-           float(random.randint(0, 100)),
-           now - datetime.timedelta(seconds=ago))
-       data.add('second',
-           float(random.randint(0, 100)),
-           now - datetime.timedelta(seconds=ago))
-
-
-
 class NewSeries(webapp2.RequestHandler):
   def get(self):
     name = self.request.get('name')
@@ -202,7 +177,7 @@ app = webapp2.WSGIApplication([
   ('/list', List),
   ('/push', Push),
   ('/newseries', NewSeries),
-  ('/random', AddRandom),  # for testing only.
+  ('/random', addrandom.AddRandom),  # for testing only.
   ('/graph/(.+)', Graph),
   ('/s/(.+)', Series)
   ])
