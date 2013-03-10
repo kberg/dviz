67f9347a6d1ce29fc46100467e5fb262cbd93bd3
mote <mote@ten.(none)>
2013-03-10 13:23:34 -0700

    got client-side data adding working again

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 "Client library to add stuff to dviz."
 
 import urllib
@@ -9,17 +10,15 @@ class DvizClient:
   def __init__(self, server=None):
     if not server:
       # TODO: later change to just main url.
-      self._URL = 'http://localhost:8080/data'
+      self._URL = 'http://localhost:8080/push'
     else:
       self._URL = server
     
-  def add(self, series_name, value, timestamp=None, user_name=None,
-      user_secret=None):
+  def add(self, series_name, value, timestamp=None, user_secret=None):
     data = {
       'series': series_name,
       'value': value,
       'timestamp': timestamp,
-      'user_name': user_name,
       'user_secret': user_secret,
       }
     pdata = urllib.urlencode(data)
@@ -30,6 +29,6 @@ class DvizClient:
 
 if __name__ == '__main__':
   dc = DvizClient()
-  print dc.add('foo', 42.0)
-  #print dc.add('foo', 45, datetime.datetime(2012, 02, 1, 9, 45))
+  value = datetime.datetime.now().second
+  print dc.add(series_name='from_client', value=value, user_secret='foo')
 
